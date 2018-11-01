a3deaf5259d715b78fadd06a808bcc958c4dd3e2
Andy Miller <miller3000@users.noreply.github.com>
2018-11-01 18:27:04 -0500

    init: Add comment to schema, model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true, length: { minimum: 3, maximum: 50}
+  validates :body, presence: true, length: { minimum: 5, maximum: 500}
 end
